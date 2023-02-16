81f2d9c833062d462f2ab99e9ba4c50739fdbce8
Fixapc <dustin@fixapc.net>
2023-02-16 06:32:42 -0600

    push

--- install.sh
+++ install.sh
@@ -490,7 +490,8 @@ starthtopinstall() {
 
 #
 loadhostnameprofile() {
-	sort -u "$basedir/configs/userdata/savedvariables.txt" >/tmp/savedvariables.txt && mv /tmp/savedvariables.txt "$basedir/configs/userdata/savedvariables.txt"
+	sort -u "$basedir/configs/userdata/savedvariables.txt" > /tmp/savedvariables.txt
+	cp -a -r -f -v /tmp/savedvariables.txt "$basedir/configs/userdata/savedvariables.txt"
 	lastconfigurationlist=$(sort -u "$basedir/configs/userdata/savedvariables.txt")
 	if [ -f "$basedir/configs/userdata/cmdline.conf.$(hostname).save" ] && [ -f "$basedir/configs/userdata/kernel.config.$(hostname).save" ]; then
 		echo -e "config files found for $(hostname) skipping first run"
@@ -498,6 +499,8 @@ loadhostnameprofile() {
 	else
 		echo -e config files not fund for "$(hostname)" generating defaults
 		echo -e "$(hostname)" >>"$basedir/configs/userdata/savedvariables.txt"
+		sort -u "$basedir/configs/userdata/savedvariables.txt" > /tmp/savedvariables.txt
+		cp -a -r -f -v /tmp/savedvariables.txt "$basedir/configs/userdata/savedvariables.txt"
 		lastconfigurationlist=$(sort -u "$basedir/configs/userdata/savedvariables.txt")
 		cp -f "$basedir/configs/cmdline_default.conf" "$basedir/configs/userdata/cmdline.conf.$(hostname).save"
 		cp -f "$basedir/configs/kernel_default.config" "$basedir/configs/userdata/kernel.config.$(hostname).save"
