a3d77359d537498c4d626fe5037810e28e30f49f
Fixapc <dustin@fixapc.net>
2023-02-16 08:09:28 -0600

    push

--- install.sh
+++ install.sh
@@ -494,17 +494,16 @@ starthtopinstall() {
 
 #
 loadhostnameprofile() {
-	lastconfigurationlist=$(sort -u "$basedir/configs/userdata/savedvariables.txt" | xargs -n1 > /tmp/savedvariables.txt && cp -a -r -f /tmp/savedvariables.txt "$basedir/configs/userdata/savedvariables.txt" && cat "$basedir/configs/userdata/savedvariables.txt")
-	sleep 10
-	echo -e "$lastconfigurationlist"
+	lastconfigurationlist=$(sort -u "$basedir/configs/userdata/savedvariables.txt" | xargs -n1 > /tmp/savedvariables.txt && cp -a -r -f /tmp/savedvariables.txt "$basedir/configs/userdata/savedvariables.txt" && cat "$basedir/configs/userdata/savedvariables.txt" && unset lastconfigurationlist)
+	cp -a -r -f /tmp/savedvariables.txt "$basedir/configs/userdata/savedvariables.txt"
 	if [ -f "$basedir/configs/userdata/cmdline.conf.$(hostname).save" ] && [ -f "$basedir/configs/userdata/kernel.config.$(hostname).save" ]; then
 		echo -e "$yellow config files found for $(hostname) skipping first run $nocolor"
-		#echo -e "$lastconfigurationlist"
+		echo -e "$lastconfigurationlist"
 	else
 		echo -e config files not fund for "$(hostname)" generating defaults
 		echo -e "$(hostname)" >> "$basedir/configs/userdata/savedvariables.txt"
 		echo -e "$yellow List Of Hostname Configs $nocolor"
-		lastconfigurationlist=$(cp -a -r -f /tmp/savedvariables.txt "$basedir/configs/userdata/savedvariables.txt" | xargs -n1)
+		lastconfigurationlist=$(sort -u "$basedir/configs/userdata/savedvariables.txt" | xargs -n1 > /tmp/savedvariables.txt && cp -a -r -f /tmp/savedvariables.txt "$basedir/configs/userdata/savedvariables.txt" && cat "$basedir/configs/userdata/savedvariables.txt")
 		echo -e "$lastconfigurationlist"
 		cp -f "$basedir/configs/cmdline_default.conf" "$basedir/configs/userdata/cmdline.conf.$(hostname).save"
 		cp -f "$basedir/configs/kernel_default.config" "$basedir/configs/userdata/kernel.config.$(hostname).save"
